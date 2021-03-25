abe7b7ce78066be396829708e5a6854b3e6adaab
VadimGlazkowUr <glazkowa.elena80.75@gmail.com>
2021-03-25 15:52:32 +0300

    Доработка

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def choice_planet(planet_name):
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
